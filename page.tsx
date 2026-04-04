e573db0ebe568e45f6196ab7953d6bea99ef49ea
ChaseTHEdrip <djamespastrana@gmail.com>
2026-04-03 19:35:41 -0700

    Add Tailwind CSS configuration with dark theme

---
+++ page.tsx
@@ -0,0 +1,17 @@
+// Tailwind CSS configuration with dark theme
+
+module.exports = {
+  darkMode: 'class', // Enable dark mode
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1d4ed8',
+        secondary: '#9333ea',
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
